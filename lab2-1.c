33979df72d0f8d4f805a4a4b433b470c77b469d6
joeunji <eunzs@naver.com>
2022-03-21 15:45:59 +0900

    commit2

--- lab2-1.c
+++ lab2-1.c
@@ -6,20 +6,22 @@ int integerType;
 float floatType;
 double doubleType;
 
-printf("Size of char: %ld byte\n",sizeof(charType));
-printf("Size of int: %ld bytes\n",sizeof(integerType));
-printf("Size of float: %ld bytes\n",sizeof(floatType));
-printf("Size of double: %ld bytes\n",sizeof(doubleType));
+printf("Size of char: %ld byte\n",sizeof(charType)); //변수 charType의 크기
+printf("Size of int: %ld bytes\n",sizeof(integerType)); //변수 integerType의 크기
+printf("Size of float: %ld bytes\n",sizeof(floatType)); //변수 floatType의 크기
+printf("Size of double: %ld bytes\n",sizeof(doubleType)); //변수 doubleType의 크기
 
 printf("-----------------------------------------\n");
-printf("Size of char: %ld byte\n",sizeof(char));
-printf("Size of int: %ld bytes\n",sizeof(int));
-printf("Size of float: %ld bytes\n",sizeof(float));
-printf("Size of double: %ld bytes\n",sizeof(double));
+
+printf("Size of char: %ld byte\n",sizeof(char)); //char의 크기
+printf("Size of int: %ld bytes\n",sizeof(int)); //integer의 크기
+printf("Size of float: %ld bytes\n",sizeof(float)); //float의 크기
+printf("Size of double: %ld bytes\n",sizeof(double)); //double의 크기
 
 printf("-----------------------------------------\n");
-printf("Size of char*: %ld byte\n",sizeof(char*));
-printf("Size of int*: %ld bytes\n",sizeof(int*));
+
+printf("Size of char*: %ld byte\n",sizeof(char*)); //char를 가리키는 포인터의 크기 (포인터는 주소값을 가지므로 다 똑같음)
+printf("Size of int*: %ld bytes\n",sizeof(int*)); 
 printf("Size of float*: %ld bytes\n",sizeof(float*));
 printf("Size of double*: %ld bytes\n",sizeof(double*));
 
--- lab2-2.c
+++ lab2-2.c
@@ -2,42 +2,47 @@
 int main()
 {
 int i;
-int *ptr;
+int *ptr; 
 int **dptr;
 i = 1234;
-printf("[checking values before ptr = &i] \n");
-printf("value of i == %d\n", i);
-printf("address of i == %p\n", &i);
-printf("value of ptr == %p\n", ptr);
-printf("address of ptr == %p\n", &ptr);
+
+printf("[checking values before ptr = &i] \n"); 
+printf("value of i == %d\n", i); //i=1234
+printf("address of i == %p\n", &i); //i의 주소
+printf("value of ptr == %p\n", ptr); //ptr이 가리키는 주소 (쓰레기값)
+printf("address of ptr == %p\n", &ptr); //ptr의 주소
+
 ptr = &i; /* ptr is now holding the address of i */
 printf("\n[checking values after ptr = &i] \n");
-printf("value of i == %d\n", i);
+printf("value of i == %d\n", i); 
 printf("address of i == %p\n", &i);
-printf("value of ptr == %p\n", ptr);
-printf("address of ptr == %p\n", &ptr);
-printf("value of *ptr == %d\n", *ptr);
+printf("value of ptr == %p\n", ptr); //ptr이 가리키는 i의 주소
+printf("address of ptr == %p\n", &ptr); //ptr의 주소
+printf("value of *ptr == %d\n", *ptr); //=i
 
 dptr = &ptr; /* dptr is now holding the address of ptr */
 printf("\n[checking values after dptr = &ptr] \n");
-printf("value of i == %d\n", i);
+printf("value of i == %d\n", i); 
 printf("address of i == %p\n", &i);
-printf("value of ptr == %p\n", ptr);
-printf("address of ptr == %p\n", &ptr);
-printf("value of *ptr == %d\n", *ptr);
-printf("value of dptr == %p\n", dptr);
-printf("address of ptr == %p\n", &dptr);
-printf("value of *dptr == %p\n", *dptr);
-printf("value of **dptr == %d\n", **dptr);
+printf("value of ptr == %p\n", ptr); //ptr이 가리키는 i의 주소
+printf("address of ptr == %p\n", &ptr); //ptr의 주소
+printf("value of *ptr == %d\n", *ptr); //=i
+printf("value of dptr == %p\n", dptr); //dptr이 가리키는 ptr의 주소
+printf("address of dptr == %p\n", &dptr); //dptr의 주소
+printf("value of *dptr == %p\n", *dptr); //=ptr 
+printf("value of **dptr == %d\n", **dptr); //=*ptr =i 
+
 *ptr = 7777; /* changing the value of *ptr */
-printf("\n[after *ptr = 7777] \n");
-printf("value of i == %d\n", i);
-printf("value of *ptr == %d\n", *ptr);
-printf("value of **dptr == %d\n", **dptr);
+printf("\n[after *ptr = 7777] \n"); 
+printf("value of i == %d\n", i); //i=*ptr=7777
+printf("value of *ptr == %d\n", *ptr); //=7777
+printf("value of **dptr == %d\n", **dptr); //**dptr=*(ptr)=i=7777
+
 **dptr = 8888; /* changing the value of **dptr */
 printf("\n[after **dptr = 8888] \n");
-printf("value of i == %d\n", i);
+printf("value of i == %d\n", i); //**dptr=*(ptr)=i=8888
 printf("value of *ptr == %d\n", *ptr);
 printf("value of **dptr == %d\n", **dptr);
+
 return 0;
 }
